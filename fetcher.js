c52714f4f897c5f425caa48c22b78b9f21e390b5
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-03 00:02:33 +0000

    pair programmed with cohort member Dexter to implement the fetcher function that takes two command line arguments and prints out the message when the file is downloaded

---
+++ fetcher.js
@@ -0,0 +1,25 @@
+const args = process.argv.slice(2);
+const request = require("request");
+const fs = require('fs');
+
+
+const path = args[1];
+const url = args[0];
+
+request(url, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+
+  fs.writeFile(path, body, err => {
+    if (err) {
+      console.error(err);
+      return;
+    } else {
+      console.log("download and saved to ./index.html");
+      process.exit();
+    }
+  });
+});
+
+
